2cedc4000ad3b97bad8db9038776100f2b0229b6
Ali Near <gribali94@gmail.com>
2021-03-28 01:29:58 +0100

    Added more controllers

--- controller.py
+++ controller.py
@@ -1,9 +1,8 @@
 from manim import *
+import os
+ASSETS_PATH = os.getcwd() + "/assets/"
 
-file = r"C:\Users\jm\Documents/ps2.svg"
-path = "C:/Users/jm/Downloads/"
-def test_func(x):
-    return np.cos(x[0] * x[1])
+
 
 def get_obj_by_id(obj, id):
     for i in obj.submobjects:
@@ -27,19 +26,27 @@ def get_objs_by_ids_rest(obj, ids):
         found = False
         for subm in obj.submobjects:
             if subm.id == i:
+                print("found", i)
                 objs.append(subm)
                 break
                 
     return (objs, rest)
 
+
 class Test(Scene):
+    """
+    This scene is for testing svg files and some transition
+    """
 
     def construct(self):
-        xbox = SVGMobject(path + "xbox_360.svg")
-        xbox_fat = SVGMobject(path + "xbox_fat.svg")
-        ps2 = SVGMobject(path + "ps2.svg")
+        xbox = SVGMobject(ASSETS_PATH + "xbox_360.svg")
+        xbox_fat = SVGMobject(ASSETS_PATH + "xbox_fat.svg")
+        ps2 = SVGMobject(ASSETS_PATH + "ps2.svg")
+        switch = SVGMobject(ASSETS_PATH + "switch.svg")
+        switch_pro = SVGMobject(ASSETS_PATH + "switch_pro.svg").scale(1.0)
         square_button_ = get_obj_by_id(ps2, "square_button")
         square_button_.set_stroke("#FF01DB", 3)
+        self.play(DrawBorderThenFill(switch))
         """
         a_button = get_obj_by_id(xbox, "a_button")
         b_button = get_obj_by_id(xbox, "b_button")
@@ -66,13 +73,10 @@ class Test(Scene):
             ]
             )
         )
-        """
         xbox_ids = ["a_button", "b_button", "x_button", "y_button", "right_analog", "left_analog"]
         ps2_ids = ["x_button", "circle_button", "square_button", "triangle_button", "right_analog", "left_analog"]
         xbox_obj, xbox_rest = get_objs_by_ids_rest(xbox, xbox_ids)
         ps2_obj, ps2_rest = get_objs_by_ids_rest(ps2, ps2_ids)
-        self.play(ShowCreation(xbox_fat))
-        """
         self.play(
             *[
                 ApplyMethod(i.shift, 2 * UP)
@@ -88,20 +92,34 @@ class Test(Scene):
             #ReplacementTransform(xbox_rest, ps2_rest)
         )
         """
+        left_con, *t = get_objs_by_ids_rest(switch, ["left_trigger", "left_con", "left_analog"])
+        screen, t = get_objs_by_ids_rest(switch, ["screen", "screen_border", "bezels", "lower_bezel"])
+        right_con = get_obj_by_id(switch, "right_con")
+        left_con = VGroup(*left_con)
+        print(len(screen))
+        screen = VGroup(*screen)
+        for i in left_con:
+            i.z_index = 2
+        right_con.z_index = 2
+        self.play(
+            ApplyMethod(left_con.shift, UP),
+            ApplyMethod(right_con.shift, UP),
+        )
+        self.play(Transform(screen, switch_pro))
+        self.play(
+            ApplyMethod(left_con.move_to, ORIGIN + .7 * LEFT),
+            ApplyMethod(right_con.move_to, ORIGIN + .7 * RIGHT),
+        )
         #self.play()
         #self.play(ReplacementTransform(xbox, ps2))
         # self.play(ReplacementTransform(ps2, nes))
         self.wait()
 
-
-class Test3D(ThreeDScene):
+class TestZIndex(Scene):
 
     def construct(self):
-        c = Cube()
-        self.play(
-            ShowCreation(c)
-        )
-        s = Sphere().set_x(-4)
-        self.add(s)
-        
-        self.move_camera(phi=20*DEGREES)
+        snes = SVGMobject(ASSETS_PATH + "xbox-one.svg")
+        ps4 = SVGMobject(ASSETS_PATH + "ps2.svg")
+        self.play(ShowCreation(snes))
+        self.play(Transform(snes, ps4))
+        self.wait()
